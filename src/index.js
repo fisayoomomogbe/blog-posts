e559ca51d9915c01744b6413d73427140e5abdea
fisayoomomogbe <doctoromo@yahoo.com>
2021-06-21 21:09:11 -0600

    created and updated index.js file for App component.

--- src/index.js
+++ src/index.js
@@ -1,17 +1,37 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import profile1 from "./images/pic-3-female.jpg"
+import profile2 from "./images/pic-2-react.jpg"
+import profile3 from "./images/pic-react-1.jpg"
+import SingleComment from "./SingleComment"
+import UserCard from "./UserCard"
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const App = () => {
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+    return (
+
+  <div className = "ui comments">
+    
+    <UserCard header = "Sarah Augustine">
+        <SingleComment name = "Sarah" date = "Today at 5pm"
+        text = "It's amazing" avatar = {profile1}/>
+    </UserCard>
+    <UserCard header = "Jack Windsow">
+        <SingleComment name = "Jack" date = "Today at 7pm"
+        text = "It's awesome"  avatar = {profile3}/>
+    </UserCard>
+    <UserCard header = "Alex Usifo">
+        <SingleComment name = "Alex" date = "Today at 10pm" 
+        text = "It's cool"  avatar = {profile2}/>
+   </UserCard>
+       
+  </div>
+
+)
+}
+
+ReactDOM.render (
+    <App/>,
+    document.querySelector("#root")
+)
